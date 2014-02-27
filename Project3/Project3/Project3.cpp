9b8cb567e25442e90e0cc369ca99ad956f9d24c5
Ross <rrydman@gmail.com>
2014-02-26 19:13:25 -0800

    1

---
+++ Project3/Project3/Project3.cpp
@@ -0,0 +1,29 @@
+// Project 3 - Linux Scheduler
+// Ross Rydman - Feb 2014
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+
+int main(){
+	// Setup file input from redirection
+	ifstream inputFile;
+	char inputFileName[] = "";
+	inFile.open(inputFileName, ios::in);
+	if (!inFile){
+		std::cerr << "Error opening file." << endl;
+		exit(1);
+	}
+	// Handle input
+	char inputexample[100]
+		while (!inFile.eof()){
+			inFile >> inputexample; // Reads one element per line
+		}
+		// Close file after done reading
+		inFile.close();
+
+		return 0;
+}
+
