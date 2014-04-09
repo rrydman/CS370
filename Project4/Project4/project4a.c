cbd4d0db6776880aa76a9395252b73dec452dd88
Ross <rrydman@gmail.com>
2014-04-09 15:11:09 -0700

    1

--- Project4/Project4/project4a.c
+++ Project4/Project4/project4a.c
@@ -4,64 +4,79 @@ Ross Rydman - (c) March 2014
 ---------------------------
 ---------------------------
 What is working:
-
+	Read input correctly
+	Queue implementation
 ---------------------------
 What isn't working:
 
 ---------------------------
-What I didn't do:
-
+What I didn't do yet:
+	Thread creation
+	Semaphore implementation
 ---------------------------
-To compile: gcc filename.cpp
+To compile: gcc filename.c -lrt
 To run: ./a.out < input.txt
 --------------------------- */
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
 
-#define BUFF_SIZE	256
+#define BUFF_SIZE	10
+#define QUEUE_SIZE  5
 
-void read_input(FILE *in){
-	if (in){
-		// Need to handle input differently from C++ - http://www.cs.utah.edu/dept/old/texinfo/glibc-manual-0.02/library_11.html
-		// Input is one int per line
-			// first line is number of nodes (lines to read)
-			// subsequent lines are node uid's
-		// Loops for each line in redirected input
-		while (in) {
-			//string line = "";
-			//getline(in, line);
-			//if (line.compare("***") == 0){
-				break;
-			//}
-			//else {
-				// Handle input
-				//int tmp;
-				//stringstream ss;
-				//ss.str(line);
-				// Read first word in line
-				//ss >> tmp;
-				// do something with temp
-				// Read rest of line
-			//}
-		}
-		//in.clear();
-	}
-	//return in;
+// Queue
+typedef struct _queue{
+	int count;
+	int head, tail;
+	int values[QUEUE_SIZE];
+} queue;
+void push(queue *q, int value){
+	q->tail = (q->tail) % QUEUE_SIZE;
+	q->values[q->tail++] = value;
+	q->count++;
 }
+int pop(queue *q){
+	q->head = (q->head) % QUEUE_SIZE;
+	q->count--;
+	return q->values[(q->head)++];
+}
+void init(queue *q){
+	q->head = q->tail = q->count = 0;
+}
+// End Queue
 
-int read_channel( /* channel */){
+typedef struct _channel{
+	queue q;
+	sem_t race_sem;
+	sem_t sync_sem;
+} channel;
+
+typedef struct _node{
+	int uid;
+	int tempuid;
+	int onehop;
+	int phase;
+	int status;  // 0 = relay ; 1 = active
+} node;
+
+// Globals
+int leaderflag;
+
+
+int read_channel(channel *chan){
 	// wait (synchronization_semaphore);
 	// wait (race_condition_semaphore);
-	int value = 0; // remove value from the queue
+	int value = pop(&chan->q); // int value = chan.queue.pop() 
 	// post (race_condition_semaphore);
 	return value;
 }
 
-void write_channel( /* channel */){
+void write_channel(channel *chan, int value){
 	// wait(race_condition_semaphore);
-	// put value on the queue
+	push(&chan->q, value);
 	// post(race_condition_semaphore);
 	// post(synchronization_semaphore);
 }
@@ -92,10 +107,26 @@ void relay_node_phase(){
 int main()
 {
 	// Handle input
-	char *inbuffer = (char *)calloc(BUFF_SIZE, sizeof(char));
-	fgets(inbuffer, BUFF_SIZE, stdin);
+	int nodecount,len,i;
+	char *linebuffer = (char *)calloc(BUFF_SIZE, sizeof(char));
+	fgets(linebuffer, BUFF_SIZE, stdin);
+	nodecount = atoi(linebuffer);
+	node *nodeptr = malloc(nodecount * sizeof(node));
+	channel *channelptr = malloc(nodecount * sizeof(channel));
+	// Create nodes
+	for (i = 0; i < nodecount; i++){
+		fgets(linebuffer, BUFF_SIZE, stdin);
+		nodeptr[i].uid = atoi(linebuffer);
+		nodeptr[i].status = 1;
+		nodeptr[i].phase= 1;
+		sem_init(&channelptr[i].race_sem, 0, 1); // Initialize Race Semaphore for channel
+		sem_init(&channelptr[i].sync_sem, 0, 0); // Initialize Sync Semaphore for channel
+		init(&channelptr[i].q); // Initialize Channel's Queue
+		printf("[%d][%d][%d]\n", nodeptr[i].phase, nodeptr[i].uid, nodeptr[i].uid); // Debugging to display input
+	}
 
-
+	free(nodeptr);
+	free(channelptr);
 	return 0;
 }
 
