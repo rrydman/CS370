f1945bd064209e9a008259be5217bfa697be0ec3
Ross <rrydman@gmail.com>
2014-04-27 23:35:19 -0700

    1

---
+++ Project6/Project6/Project6a.c
@@ -0,0 +1,28 @@
+/*
+----------------------------------------------------
+CS370 Project 6 - Sockets
+Ross Rydman - (c) April 2014
+----------------------------------------------------
+To compile: gcc filename.c
+To run: ./a.out <0|1>
+----------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdlib.h>
+
+// Constants
+#define SERVER 0
+#define CLIENT 1
+
+// Globals
+
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
